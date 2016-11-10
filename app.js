4ac20aed091cd478edb995c0e7fbc4b8b1407fae
AkhilHector <akhilhector.1@gmail.com>
2016-11-11 01:14:33 +0530

    Cleanup of the website - Part 4
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- app.js
+++ app.js
@@ -10,63 +10,68 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 module.exports.unleash = function() {
-        var port = process.env.PORT || 1337;
-        var app = express();
-        var router = express.Router();
-        var errorPage = fs.readFileSync("404.html", "UTF-8");
+    var port = process.env.PORT || 1337;
+    var app = express();
+    var router = express.Router();
+    var errorPage = fs.readFileSync("404.html", "UTF-8");
 
-        app.use(express.static('assets'));
-        app.set('title', "home | akhil");
-        app.use(cookieParser());
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static('assets'));
+    app.set('title', "home | akhil");
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-        router.get('/', function(req, res) {
-                res.sendFile(__dirname + '/templates/index.html');
+    router.get('/', function(req, res) {
+            res.sendFile(__dirname + '/templates/index.html');
+    });
+
+    router.get('/about', function(req, res) {
+            res.sendFile(__dirname + '/templates/about.html');
+    });
+
+    router.get('/blog', function(req, res) {
+        res.sendFile(__dirname + '/templates/blogpages/0.html');
+    });
+
+    router.get('/blog/page/:pageno', function (req, res) {
+        var pagenum = req.params.pageno
+        res.sendFile(__dirname + '/templates/blogpages/' + pagenum + '.html')
+    })
+
+    router.get('/rhetorics', function(req, res) {
+    		res.sendFile(__dirname + '/templates/rhetorics.html');
+    });
+
+    router.get('/snaps', function(req, res) {
+            res.sendFile(__dirname + '/templates/snaps.html');
+    });
+
+    router.get('/[0-9]', function(req, res) {
+            res.redirect(errorPage);
+    });
+
+    router.get('*', function(req, res) {
+        var match = '/' + req.params[0] + '.html';
+        fs.exists(match, function(present) {
+                if(present) {
+                    fs.readFile(match, function(err, data) {
+                            if(err) {
+                                    res.send(errorPage.toStrng(), "UTF-8");
+                            }
+                            else {
+                                    res.end(data, "UTF-8");
+                            }
+                    });
+                }
+                else {
+                    res.end(errorPage.toString(), "UTF-8");
+                }
         });
+    });
 
-        router.get('/about', function(req, res) {
-                res.sendFile(__dirname + '/templates/about.html');
-        });
+    app.use('/', router);
 
-        router.get('/blog', function(req, res) {
-            res.sendFile(__dirname + '/templates/blog.html');
-        });
-
-        router.get('/rhetorics', function(req, res) {
-        		res.sendFile(__dirname + '/templates/rhetorics.html');
-        });
-
-        router.get('/snaps', function(req, res) {
-                res.sendFile(__dirname + '/templates/snaps.html');
-        });
-
-        router.get('/[0-9]', function(req, res) {
-                res.redirect(errorPage);
-        });
-
-        router.get('*', function(req, res) {
-                var match = '/' + req.params[0] + '.html';
-                fs.exists(match, function(present) {
-                        if(present) {
-                                fs.readFile(match, function(err, data) {
-                                        if(err) {
-                                                res.send(errorPage.toStrng(), "UTF-8");
-                                        }
-                                        else {
-                                                res.end(data, "UTF-8");
-                                        }
-                                });
-                        }
-                        else {
-                                res.end(errorPage.toString(), "UTF-8");
-                        }
-                });
-        });
-
-        app.use('/', router);
-
-        http.createServer(app).listen(port, function() {
-                console.log("Front End Application Server started");
-        });
+    http.createServer(app).listen(port, function() {
+            console.log("Front End Application Server started");
+    });
 }
